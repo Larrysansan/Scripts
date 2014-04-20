fed41fe321b1090d5d4e02038a7d7460eaf7e204
kevvv <larcer.kevin@gmail.com>
2014-04-20 01:01:19 -0400

    Update ButtonTest.lua

--- ButtonTest.lua
+++ ButtonTest.lua
@@ -4,16 +4,26 @@ function OnLoad()
 	PrintChat("Button Tester")
 	TestButton = Button(GetSprite("buttonUp.dds"), GetSprite("buttonDown.dds"), 100, 100, 200, 100)
 	TestButton:SetAlpha(220)
-	TestButton:RemoveToggle(0.100)
-	TestButton2 = Button(GetSprite("buttonUp.dds"), GetSprite("buttonDown.dds"), 200, 200, 200, 100)
+	TestButton2 = Button(GetSprite("circleUp.dds"), GetSprite("circleDown.dds"), 200, 200, 20, 20)
+	TestButton2:Make3D(3046.87, -188.57, 4297.29)
+	TestButton2:RemoveToggle(0.100)
 	TestButton2:SetAlpha(200)
+	TestButton2:AddFunction(MessageMe)
 	--TestButton2:RemoveToggle(0.100)
 	shiftDown = false
 end
 
 function OnDraw()
-	TestButton:Draw()
-	TestButton2:Draw()
+	local cursorPos = GetCursorPos()
+	DrawText("X = " .. string.format("%.2f", mousePos.x),16, cursorPos.x, cursorPos.y + 50, 0xFF00FF00)
+	DrawText("Y = " .. string.format("%.2f", mousePos.y),16, cursorPos.x, cursorPos.y + 65, 0xFF00FF00)
+	DrawText("Z = " .. string.format("%.2f", mousePos.z),16, cursorPos.x, cursorPos.y + 80, 0xFF00FF00)
+	--TestButton:Draw()
+	--TestButton2:Draw()
+end
+
+function MessageMe()
+	PrintChat("Button has been pressed")
 end
 
 function OnWndMsg(msg, key)
